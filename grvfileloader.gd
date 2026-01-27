8c255b42edb5408aae3d8816e78c6f767f38b5a1
Orange-Gaming-System <erikmathiasanvin@gmail.com>
2026-01-27 15:55:43 -0800

    .grv File Loader Rewrite Rewrote the entire .grv File Loader: Uses a more logical, command-based system to parse the data.

--- grvfileloader.gd
+++ grvfileloader.gd
@@ -1,7 +1,6 @@
 extends Node
 
 var gamecached = false
-var gamecachefailure = ""
 var levelcount: int = -1
 var title = ""
 var byline = ""
@@ -9,57 +8,41 @@ var author = ""
 var mappaths = []
 
 func cachegrvfile(path): # stores all the data about a game from the .grv file into variables for easy access.
-	gamecached = false # game is not cached
-	gamecachefailure = "" # caching has not failed
+	gamecached = true # game is cached
 	var file = FileAccess.open(path, FileAccess.READ) # gets the .grv file
 	var filecontent = file.get_as_text() # loads the contents of the file
 	var game = filecontent.split("\n", false) # converts the file into an array, line by line
-	var commands = [] # holds all the commands
-	var args = [] # holds the arguements
+	# Set all variables to their default value.
+	levelcount = 75
+	title = "Custom Game"
+	byline = ""
+	author = ""
+	mappaths = [].resize(75)
+	# Loop through every line in the file.
 	for line in game:
 		# For every line, this loop splits it into commands and arguements.
 		var linedata = line.split(" ", true, 1)
-		commands.append(linedata[0])
-		args.append(linedata[1])
-	var titlepos = commands.find("title") # gets the position of the title in the array.
-	if titlepos != -1: # if there is a title, set the title to it. else, says "No Title Added"
-		title = args[titlepos].trim_prefix("\"").trim_suffix("\"")
-	else:
-		title = "No Title Added"
-	var bylinepos = commands.find("byline") # repeat what just happened for the title but for the byline.
-	if bylinepos != -1:
-		byline = args[bylinepos].trim_prefix("\"").trim_suffix("\"")
-	else: # the byline is set to blank when there is none.
-		byline = ""
-	var authorpos = commands.find("author") # repeat for the author.
-	if authorpos != -1:
-		author = args[authorpos].trim_prefix("\"").trim_suffix("\"")
-	else: # author is set to "Unknown" when no author is specified.
-		author = "Unknown"
-	var levelcountpos = commands.find("levels") # get the level count position.
-	if levelcountpos != -1:
-		levelcount = int(args[levelcountpos]) # if there is one, store it.
-		gamecached = true
-	else:
-		levelcount = -1 # else, return with an error.
-		gamecachefailure = "nolevelcount"
-		print("Game cache failed: No Level Count.")
-		return
-	mappaths.resize(levelcount) # set the size of the "mappaths" array to the level count.
-	for n in commands.size(): # this loop runs for every line in the .grv file.
-		var command = commands[n]
-		if command != "map": # if the command does not specify a map, ignore this line.
-			continue
-		var arg = args[n]
-		var mapargs = arg.split(" ", true, 1) # split the map into a map number and a map path.
-		if "-" in mapargs[0]: # checks if this is for 1 map or a range of maps.
-			var maprange = mapargs[0].split("-", false, 1) # if it is a range, determine the top and bottom.
-			for map in int(maprange[1]) - int(maprange[0]) + 1: # for each map in the range, store them in their respective location in the array.
-				map += int(maprange[0])
-				mappaths[map-1] = "/".join([path.rsplit("/", true, 1)[0], mapargs[1].trim_prefix("\"").trim_suffix("\"")])
-		else:
-			mappaths[int(mapargs[0])-1] = "/".join([path.rsplit("/", true, 1)[0], mapargs[1].trim_prefix("\"").trim_suffix("\"")]) # if this is a single map, store its path in the correct location in the array.
-	return # when debugging or testing, place a breakpoint here to check if the code executed correctly.
+		# Check the command (forced lowercase to make the loop not case-sensitive) of this line, if it is not valid, ignore it.
+		match linedata[0].to_lower():
+			"title": # For title, byline, and author, set the respective variable and remove all quotation marks.
+				title = linedata[1].trim_prefix("\"").trim_suffix("\"")
+			"byline":
+				byline = linedata[1].trim_prefix("\"").trim_suffix("\"")
+			"author":
+				author = linedata[1].trim_prefix("\"").trim_suffix("\"")
+			"levels": # For levels, set the level count to the argument after converting it to an integer, and set the size of the mappaths array.
+				levelcount = int(linedata[1])
+				mappaths.resize(levelcount)
+			"map": # For the map command, this code stores all the paths for the maps.
+				var arg = linedata[1]
+				var mapargs = arg.split(" ", true, 1) # split the map into a map number and a map path.
+				if "-" in mapargs[0]: # checks if this is for 1 map or a range of maps.
+					var maprange = mapargs[0].split("-", false, 1) # if it is a range, determine the top and bottom.
+					for map in int(maprange[1]) - int(maprange[0]) + 1: # for each map in the range, store them in their respective location in the array.
+						map += int(maprange[0])
+						mappaths[map-1] = "/".join([path.trim_suffix("/"), mapargs[1].trim_prefix("\"").trim_suffix("\"")])
+				else:
+					mappaths[int(mapargs[0])-1] = "/".join([path.trim_suffix("/"), mapargs[1].trim_prefix("\"").trim_suffix("\"")]) # if this is a single map, store its path in the correct location in the array.
 
 func loadlevelfile(level: int):
 	var lvlpath = mappaths[level - 1]
