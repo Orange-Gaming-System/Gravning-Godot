7f9f16a146a51bdcd8ff1510a0da47241a5752b8
Orange-Gaming-System <erikmathiasanvin@gmail.com>
2026-02-07 21:17:59 -0800

    New Dirt Texture! Updated Dirt Texture, Added a BG Texture, Added an apple texture.

--- level_builder.gd
+++ level_builder.gd
@@ -83,6 +83,6 @@ func build_level(map: Map):
 	generate_objs(map.objs)
 
 func _ready():
-	var map = Map.new("/home/erik/Grv Levels/gdtest.grvmap")
+	var map = Map.new("/home/erik/Grv Levels/gdtest2.grvmap")
 	build_level(map)
 	
