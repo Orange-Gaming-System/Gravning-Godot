fde733ac2618fa8cc9c0fb26784c5f2b8b055bf6
Orange-Gaming-System <erikmathiasanvin@gmail.com>
2026-02-09 17:28:00 -0800

    Attempted fix of ghost movement.

--- ghost.gd
+++ ghost.gd
@@ -1,5 +1,13 @@
 class_name Ghost extends Character
 
+func _ready():
+    super._ready()
+    process_priority = map_tile.prio
+
+func sort_closer_player(a, b):
+    var player_pos = map_tile.map.player.xy
+    return a.distance_squared_to(player_pos) < b.distance_squared_to(player_pos)
+
 func _new_tick():
     board_pos = goal_pos
     start_pos = board_pos
@@ -8,24 +16,17 @@ func _new_tick():
     possible_movements[1].x -= 1
     possible_movements[2].y += 1
     possible_movements[3].y -= 1
-    var allowed_movements = []
+    possible_movements.shuffle()
+    possible_movements.sort_custom(sort_closer_player)
+    var chosen_movement = board_pos
     for movement in possible_movements:
         if GameManager.get_movement_type(movement, board_pos) == GameManager.MOVE_TYPE.EMPTY:
-            var goal_obj = map_tile.map.at(movement)
-            if goal_obj.item.type == Item.Type.PLAYER or goal_obj.item.type == Item.Type.EMPTY:
-                allowed_movements.append(movement)
-    var movement_distances = []
-    var player_pos = map_tile.map.player.xy
-    for movement in allowed_movements:
-        movement_distances.append(movement.distance_squared_to(player_pos))
-    var smallests = []
-    for i in movement_distances.size():
-        if movement_distances[i] == movement_distances.min():
-            smallests.append(i)
-    goal_pos = allowed_movements[smallests.pick_random()]
+            var goal_tile = map_tile.map.at(movement)
+            if goal_tile.item.type == Item.Type.PLAYER or goal_tile.item.type == Item.Type.EMPTY:
+                chosen_movement = movement
+                break
+    goal_pos = chosen_movement
     super._new_tick()
-        
-    
 
 func _collided(area):
     if area.get_parent() is Player:
