5250c3481dc825a012805f79d5fedaf9e13fd85b
H. Peter Anvin <hpa@zytor.com>
2026-02-24 18:59:50 -0800

    gitversion.gd: tidy up the code
    - Make the code cleaner by keeping the name, type, and format code
      information together in an array.

--- version/gitversion.gd
+++ version/gitversion.gd
@@ -1,22 +1,36 @@
 extends RefCounted
 
+const gitinfo : Array = [
+    [ "author_time", "int", "%at" ],
+    [ "author_date", "str", "%ai" ],
+    [ "commit_time", "int", "%ct" ],
+    [ "commit_date", "str", "%ci" ],
+    [ "commit",      "str", "%H" ],
+    [ "describe",    "str", "%(describe:abbrev=6)" ],
+    [ "subject",     "str", "%s" ]
+]
+
 static func version():
     var ver : Dictionary = { }
     var output : Array
-    OS.execute("git",
-        PackedStringArray(["log", "--max-count=1",
-        r"--pretty=tformat:%at%n%ct%n%ai%n%ci%n%H%n%(describe:abbrev=6)%n%s"]),
-        output)
+    var fmt : String = "--pretty=tformat:"
+    for gi in gitinfo:
+        fmt += gi[2] + "%n"
+    OS.execute("git", PackedStringArray(["log", "--max-count=1", fmt]), output)
     if !output.is_empty():
-        var fields = output[0].split("\n")
-        var i = 0
-        for iarg in ["author_time", "commit_time"]:
-            ver[iarg] = int(fields[i])
-            i += 1
-        for sarg in ["author_date", "commit_date", "commit",
-                     "describe", "subject"]:
-            ver[sarg] = fields[i]
-            i += 1
+        var fields : PackedStringArray = output[0].split("\n")
+        for i in gitinfo.size():
+            var field : String
+            if i < fields.size():
+                field = fields[i]
+            var gi = gitinfo[i]
+            match gi[1]:
+                "int":
+                    if field.is_valid_int():
+                        ver[gi[0]] = int(field)
+                "str":
+                    ver[gi[0]] = field
+
     if !ver:
         push_error("Failed to get version from git log. Make sure you have git installed and project is inside a valid git directory.")
     return ver
